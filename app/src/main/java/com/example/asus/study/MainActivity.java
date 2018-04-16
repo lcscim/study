12e863c1ed04a58dcda8abd91f7e9bee0b4b1646
lcscim <you@example.com>
2018-04-16 22:11:31 +0800

    home 0416 commit.

--- app/src/main/java/com/example/asus/study/MainActivity.java
+++ app/src/main/java/com/example/asus/study/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.asus.study;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -47,4 +46,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
 }
